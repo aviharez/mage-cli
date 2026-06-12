68112dd2036fd42b1acedf0c63430b8e018793ce
Syifa Nurzain <elzainsyifa@gmail.com>
2026-06-12 18:10:01 +0700

    feat(docs): add new troubleshoot info, update version to 1.2.4

--- packages/web/config.mjs
+++ packages/web/config.mjs
@@ -7,7 +7,7 @@ export default {
   socialCard: "",
   github,
   discord: "",
-  version: "1.2.3",
+  version: "1.2.4",
   headerLinks: [
     { name: "app.header.docs",      url: "/docs/" },
     { name: "app.header.catalog",   url: "/hub/" },
